4594a768016e7604bc88d54ee3625c8b181baba8
zhilu1 <z5096599@student.unsw.edu.au>
2019-01-03 09:39:09 +0800

    first commit 创建spring boot项目

---
+++ src/main/java/com/practice/management/ManagementApplication.java
@@ -0,0 +1,14 @@
+package com.practice.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementApplication.class, args);
+    }
+
+}
+
