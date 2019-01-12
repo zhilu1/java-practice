8e0d0481e2d0c3bff22eb3634840e16780c174b5
zhilu1 <z5096599@student.unsw.edu.au>
2019-01-12 14:19:39 +0800

    start calendar

--- src/test/resources/sql/init.sql
+++ src/test/resources/sql/init.sql
@@ -1,4 +1,4 @@
--- DROP TABLE IF EXISTS `Sys_User`;
+DROP TABLE IF EXISTS `Sys_User`;
 CREATE TABLE `Sys_User`(
   `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(200) NOT NULL,
@@ -9,7 +9,7 @@ CREATE TABLE `Sys_User`(
   UNIQUE (`username`)
 )CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- DROP TABLE IF EXISTS `record`;
+DROP TABLE IF EXISTS `record`;
 CREATE TABLE `record` (
   `id` int(30) NOT NULL AUTO_INCREMENT,
   `riqi` date NOT NULL,
@@ -55,6 +55,16 @@ CREATE TABLE `Sys_permission_role`(
   PRIMARY KEY (`id`)
 )CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+DROP TABLE IF EXISTS `office_time_calendar`;
+CREATE TABLE `office_time_calendar` (
+  `id` bigint(20) NOT NULL,
+  `day` date NOT NULL,
+  `start_time` time DEFAULT NULL,
+  `end_time` time DEFAULT NULL,
+  `status` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`)
+)CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 insert into SYS_USER (id,username, password, name, department) values (1,'101', 'admin', 'Oven', 'UNO');
 insert into SYS_USER (id,username, password, name, department) values (2,'111', 'abel', 'Alien', 'UFO');
 
@@ -69,8 +79,10 @@ insert into SYS_ROLE_USER(user_id,role_id) values(2,2);
 
 INSERT INTO `Sys_permission` VALUES ('1', 'ROLE_HOME', 'home page', '/', null), ('2', 'ROLE_ADMIN', 'empty admin page', '/admin', null), ('3', 'ROLE_USEROP', 'user operations', '/authority/*', null);
 INSERT INTO `Sys_permission` VALUES ('4', 'ROLE_ROLEOP', 'role operations', '/role/*', null);
+INSERT INTO `sys_permission` VALUES (5, 'ROLE_IMPORTOP', 'import record', '/import', NULL);
+INSERT INTO `sys_permission` VALUES (6, 'ROLE_EXPORTOP', 'export record', '/export', NULL);
+INSERT INTO `sys_permission` VALUES (7, 'ROLE_SELECTOP', 'select staff record', '/selectRecordByIdAndDate', NULL);
+
+
 INSERT INTO `Sys_permission_role` VALUES ('1', '1', '1'), ('2', '1', '2'), ('3', '2', '1') , ('4', '1', '3'), ('5', '1', '4');
-
-
-
 
